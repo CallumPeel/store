f3ed991614fb9e61524d58fe5c04717350e46d5c
Slinky <callum.epeel@gmail.com>
2023-09-27 16:43:03 +0800

    Refactored store and item into dp.py
    some get/post requests updated; not all.

--- app.py
+++ app.py
@@ -1,30 +1,20 @@
+import uuid
 from flask import Flask, request
-
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
-
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
-    request.data = request.get_json()
-    new_store = {"name": request.data["name"], "items": []}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
     return new_store, 201
 
 
@@ -39,12 +29,12 @@ def create_item(name):
     return {"message": "Store not found"}, 404
 
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Error, Store not found"}, 404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Error, Store not found"}, 404
 
 @app.get("/store/<string:name>/item")
 def get_item_in_store(name):
