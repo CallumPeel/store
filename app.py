a5f3c27e8073d24d2d6a65cc725de0687e7f0201
Slinky <callum.epeel@gmail.com>
2023-09-27 10:26:47 +0800

    Get and Post Requests
    Get Store, get item, post store, post item, etc.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -16,5 +16,39 @@ stores = [
 ]
 
 @app.get("/store")
-def getStores():
+def get_stores():
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request.data = request.get_json()
+    new_store = {"name": request.data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request.data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request.data["name"], "price": request.data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Error, Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
