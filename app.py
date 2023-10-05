e7ea1235311ff79dc98f5d9792396af3afd96ea1
Callum Peel <callum.epeel@gmail.com>
2023-10-05 20:22:02 +0800

    Put and Del requests added

--- app.py
+++ app.py
@@ -50,6 +50,28 @@ def get_item(item_id):
         abort(404, message="404 not found.")
 
 
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        return abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in JSON payload.")
+    
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        return {"message": "Item not found"}
+
 # Store APIs ***********************************************************
 
 @app.post("/store")
@@ -80,3 +102,10 @@ def get_store(store_id):
     except KeyError:
         abort(404, message="404 not found.")
 
+@app.delete("/store/<string:store_id>")
+def delete(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        return abort(404, message="Store not found.")
